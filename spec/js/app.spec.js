0071fc6250751501e497840b12e8c6ef7d7aec18
marcbgold <marcbgold@yahoo.com>
2018-03-09 16:04:08 -0500

    made details button change text when clicked

--- spec/js/app.spec.js
+++ spec/js/app.spec.js
@@ -4,9 +4,9 @@ describe('portfolio website', function() {
 
 		it('removes hidden class from element on button click', function() {
 			const testLink = document.createElement('a')
-			testLink.className = 'hidden';
+			testLink.className = 'hidden'
 			
-			toggleElementDisplay(testLink);
+			toggleElementDisplay(testLink)
 
 			expect(testLink.classList.contains('hidden')).toEqual(false);
 		})
@@ -14,9 +14,31 @@ describe('portfolio website', function() {
 		it('adds hidden class to nav links on button click', function() {
 			const testLink = document.createElement('a')
 
-			toggleElementDisplay(testLink);
+			toggleElementDisplay(testLink)
 
 			expect(testLink.classList.contains('hidden')).toEqual(true);
+		})
+
+	})
+
+	describe('changes text on project details button when clicked', function() { 
+
+		it('changes text from show details to hide details', function() {
+			const testButton = document.createElement('button')
+			testButton.innerText = 'Show Details'
+
+			changeButtonText(testButton)
+
+			expect(testButton.innerText).toEqual('Hide Details');
+		})
+
+		it('changes text from hide details to show details', function() {
+			const testButton = document.createElement('button')
+			testButton.innerText = 'Hide Details'
+
+			changeButtonText(testButton)
+
+			expect(testButton.innerText).toEqual('Show Details');
 		})
 
 	})	
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 function toggleElementDisplay(element) {
 	if (element) {
-		element.classList.toggle('hidden');
+		element.classList.toggle('hidden')
 	}
 }
 
@@ -8,14 +8,26 @@ const navIcon = document.querySelector('.nav-icon')
 const navLinks = document.querySelector('.nav-links')
 if (navIcon) {
 	navIcon.addEventListener('click', function() {
-		toggleElementDisplay(navLinks);
-})}
+		toggleElementDisplay(navLinks)
+	})
+}
+
+function changeButtonText(button) {
+	if (button) {
+		if (button.innerText === 'Show Details') {
+			button.innerText = 'Hide Details'
+		} else {
+			button.innerText = 'Show Details'
+		}
+	}
+}
 
 const projectDetailsButtons = document.querySelectorAll('.project-details-button')
 projectDetailsButtons.forEach(function(button) {
 	if (button) {
 		button.addEventListener('click', function() {
-			toggleElementDisplay(this.parentNode.parentNode.children[1]);
+			changeButtonText(this)
+			toggleElementDisplay(this.parentNode.parentNode.children[1])
 		})
 	}
 })
